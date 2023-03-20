8ec0b64486d9f8622802cc0e9679709f1b56856c
Alex Sheehan <asheehan@edx.org>
2023-03-20 16:03:29 +0000

    feat: added subsidy content metadata client fetch method

--- edx_enterprise_subsidy_client/client.py
+++ edx_enterprise_subsidy_client/client.py
@@ -2,6 +2,7 @@
 API client for interacting with the enterprise-subsidy service.
 """
 import logging
+from urllib.parse import urljoin
 
 import requests  # pylint: disable=unused-import
 from django.conf import settings
@@ -15,7 +16,6 @@ class EnterpriseSubsidyAPIClient:
     API client for calls to the enterprise-subsidy service.
 
     To use this within your service, ensure the service's settings contain the following vars:
-    SOCIAL_AUTH_EDX_OAUTH2_URL_ROOT=root-url-for-oauth-2
     BACKEND_SERVICE_EDX_OAUTH2_KEY=your-services-application-key
     BACKEND_SERVICE_EDX_OAUTH2_SECRET=your-services-application-secret
     ENTERPRISE_SUBSIDY_URL=enterprise-subsidy-service-base-url
@@ -24,16 +24,41 @@ class EnterpriseSubsidyAPIClient:
     API_BASE_URL = settings.ENTERPRISE_SUBSIDY_URL.strip('/') + '/api/v1/'
     SUBSIDIES_ENDPOINT = API_BASE_URL + 'subsidies/'
     TRANSACTIONS_ENDPOINT = API_BASE_URL + 'subsidies/{subsidy_uuid}/transactions/'
+    CONTENT_METADATA_ENDPOINT = API_BASE_URL + 'content-metadata/'
 
     def __init__(self):
         """
         Initializes the OAuthAPIClient instance.
         """
         self.client = OAuthAPIClient(
-            settings.SOCIAL_AUTH_EDX_OAUTH2_URL_ROOT.strip('/'),
-            settings.BACKEND_SERVICE_EDX_OAUTH2_KEY,
-            settings.BACKEND_SERVICE_EDX_OAUTH2_SECRET,
+            settings.ENTERPRISE_BACKEND_SERVICE_EDX_OAUTH2_PROVIDER_URL.strip('/'),
+            settings.ENTERPRISE_BACKEND_SERVICE_EDX_OAUTH2_KEY,
+            settings.ENTERPRISE_BACKEND_SERVICE_EDX_OAUTH2_SECRET,
         )
+
+    def get_content_metadata_url(self, content_identifier):
+        """
+        Helper method to generate the subsidy service metadata API url
+        """
+        return urljoin(self.CONTENT_METADATA_ENDPOINT, content_identifier)
+
+    def get_subsidy_content_data(self, enterprise_uuid, content_identifier):
+        """
+        Client method to fetch enterprise specific content data (ie price and product source) from the subsidy service.
+        Content identifier can be either content key or content uuid.
+        """
+        try:
+            resp = self.client.get(
+                self.get_content_metadata_url(content_identifier) +
+                f"?enterprise_customer_uuid={enterprise_uuid}"
+            )
+            response_data = resp
+            resp.raise_for_status()
+        except requests.exceptions.HTTPError as exc:
+            if exc.request.status_code == 404:
+                logger.error(f'Failed to fetch subsidy data- 404 content not found')
+            raise exc
+        return response_data.json()
 
     def list_subsidies(self, enterprise_uuid=None, subsidy_type=None):
         """
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,10 @@
 """
 Tests for edx_enterprise_subsidy_client.py.
 """
+from unittest import mock
+
 from edx_enterprise_subsidy_client import EnterpriseSubsidyAPIClient
+from test_utils.utils import MockResponse
 
 
 def test_client_init():
@@ -10,3 +13,26 @@ def test_client_init():
     """
     subsidy_client = EnterpriseSubsidyAPIClient()
     assert subsidy_client is not None
+
+
+@mock.patch('edx_enterprise_subsidy_client.client.OAuthAPIClient', return_value=mock.MagicMock())
+def test_client_fetch_subsidy_content_data_success(
+    mock_oauth_client,
+):
+    """
+    Test the client's ability to handle api requests to fetch subsidy content metadata from the subsidy service
+    """
+    course_key = 'edX+DemoX'
+    mocked_data = {
+        'content_uuid': '484ad134-8004-43b3-ad56-b57c83e4ba24',
+        'content_key': course_key,
+        'source': 'edX',
+        'content_price': '149.00'
+    }
+    mock_oauth_client.return_value.get.return_value = MockResponse(mocked_data, 200)
+    subsidy_service_client = EnterpriseSubsidyAPIClient()
+    response = subsidy_service_client.get_subsidy_content_data(
+        enterprise_uuid='6d3ad134-8004-43b3-ad56-c57c83e4ea23',
+        content_identifier=course_key
+    )
+    assert response == mocked_data
