f08c5998eca9d42ed39283edbe9e1c3511f05eca
Alex Dusenbery <adusenbery@edx.org>
2023-03-24 11:54:59 -0400

    feat: add implementation for many of the client methods.  TODO later: unit tests.
    ENT-6962

--- edx_enterprise_subsidy_client/__init__.py
+++ edx_enterprise_subsidy_client/__init__.py
@@ -2,6 +2,6 @@
 Client for interacting with the enterprise-subsidy service..
 """
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .client import EnterpriseSubsidyAPIClient
--- edx_enterprise_subsidy_client/client.py
+++ edx_enterprise_subsidy_client/client.py
@@ -24,7 +24,7 @@ class EnterpriseSubsidyAPIClient:
 
     API_BASE_URL = settings.ENTERPRISE_SUBSIDY_URL.strip('/') + '/api/v1/'
     SUBSIDIES_ENDPOINT = API_BASE_URL + 'subsidies/'
-    TRANSACTIONS_ENDPOINT = API_BASE_URL + 'subsidies/{subsidy_uuid}/transactions/'
+    TRANSACTIONS_ENDPOINT = API_BASE_URL + 'transactions/'
     CONTENT_METADATA_ENDPOINT = API_BASE_URL + 'content-metadata/'
 
     def __init__(self):
@@ -39,14 +39,14 @@ class EnterpriseSubsidyAPIClient:
 
     def get_content_metadata_url(self, content_identifier):
         """Helper method to generate the subsidy service metadata API url."""
-        return urljoin(self.CONTENT_METADATA_ENDPOINT, content_identifier)
+        return self.CONTENT_METADATA_ENDPOINT + content_identifier
 
-    def get_subsidy_content_data(self, enterprise_uuid, content_identifier):
+    def get_subsidy_content_data(self, enterprise_customer_uuid, content_identifier):
         """
         Client method to fetch enterprise specific content data.
 
         Args:
-            enterprise_uuid (str): Enterprise customer UUID
+            enterprise_customer_uuid (str): Enterprise customer UUID
             content_identifier (str): Either content key or UUID for the associated content to be fetched
         Returns:
             json subsidy content data response:
@@ -59,8 +59,8 @@ class EnterpriseSubsidyAPIClient:
         """
         try:
             resp = self.client.get(
-                self.get_content_metadata_url(content_identifier) +
-                f"?enterprise_customer_uuid={enterprise_uuid}"
+                self.get_content_metadata_url(content_identifier),
+                params={'enterprise_customer_uuid': enterprise_customer_uuid}
             )
             response_data = resp
             resp.raise_for_status()
@@ -70,37 +70,100 @@ class EnterpriseSubsidyAPIClient:
             raise exc
         return response_data.json()
 
-    def list_subsidies(self, enterprise_uuid=None, subsidy_type=None):
+    def list_subsidies(self, enterprise_customer_uuid):
         """
-        TODO: add docstring.
+        Client method to list enterprise subsidy records for the given enterprise_customer_uuid.
+
+        Args:
+            enterprise_customer_uuid (str): Enterprise customer UUID
+        Returns:
+            Paginated response of serialized Subsidy records:
+            ```
+            {
+                "count": 123,
+                "next": "http://api.example.org/accounts/?page=4",
+                "previous": "http://api.example.org/accounts/?page=2",
+                "results": [
+                  {
+                    "uuid": "095be615-a8ad-4c33-8e9c-c7612fbf6c9f",
+                    "title": "string",
+                    "enterprise_customer_uuid": "fdfda46b-94d9-42dc-a755-6e2ed472a413",
+                    "active_datetime": "2019-08-24T14:15:22Z",
+                    "expiration_datetime": "2019-08-24T14:15:22Z",
+                    "unit": "usd_cents",
+                    "reference_id": "string",
+                    "reference_type": "opportunity_product_id",
+                    "current_balance": "string"
+                  }
+                ]
+              }
+            ```
         """
-        raise NotImplementedError
+        response = self.client.get(
+            self.SUBSIDIES_ENDPOINT,
+            params={'enterprise_customer_uuid': enterprise_customer_uuid}
+        )
+        response.raise_for_status()
+        return response.json()
 
     def retrieve_subsidy(self, subsidy_uuid):
         """
         TODO: add docstring.
         """
-        raise NotImplementedError
+        response = self.client.get(
+            self.SUBSIDIES_ENDPOINT + f'{subsidy_uuid}/'
+        )
+        response.raise_for_status()
+        return response.json()
 
     def list_subsidy_transactions(
-        self, subsidy_uuid, include_aggregates=True, user_id=None, content_key=None,
+        self, subsidy_uuid, include_aggregates=True, lms_user_id=None, content_key=None
     ):
         """
         TODO: add docstring.
         """
-        raise NotImplementedError
+        query_params = {'subsidy_uuid': subsidy_uuid}
+        if include_aggregates:
+            query_params['include_aggregates'] = include_aggregates
+        if lms_user_id:
+            query_params['lms_user_id'] = lms_user_id
+        if content_key:
+            query_params['content_key'] = content_key
+      
+        response = self.client.get(
+            self.TRANSACTIONS_ENDPOINT,
+            params=query_params,
+        )
+        response.raise_for_status()
+        return response.json()
 
-    def retrieve_subsidy_transaction(self, subsidy_uuid, transaction_uuid):
+    def retrieve_subsidy_transaction(self, transaction_uuid):
         """
         TODO: add docstring.
         """
-        raise NotImplementedError
+        response = self.client.get(
+            self.TRANSACTIONS_ENDPOINT + f'{transaction_uuid}/'
+        )
+        response.raise_for_status()
+        return response.json()
 
-    def create_subsidy_transaction(self, subsidy_uuid, user_id, content_key, **kwargs):
+    def create_subsidy_transaction(self, subsidy_uuid, lms_user_id, content_key, subsidy_access_policy_uuid):
         """
         TODO: add docstring.
         """
-        raise NotImplementedError
+        request_payload = {
+            'subsidy_uuid': subsidy_uuid,
+            'learner_id': lms_user_id,
+            'content_key': content_key,
+            'subsidy_access_policy_uuid': subsidy_access_policy_uuid,
+        }
+        response = self.client.post(
+            self.TRANSACTIONS_ENDPOINT,
+            json=request_payload,
+        )
+        print(response.json())
+        response.raise_for_status()
+        return response.json()
 
     def reverse_subsidy_transaction(self, subsidy_uuid, transaction_uuid):
         """
@@ -108,8 +171,17 @@ class EnterpriseSubsidyAPIClient:
         """
         raise NotImplementedError
 
-    def can_redeem(self, subsidy_uuid, user_id, content_key):
+    def can_redeem(self, subsidy_uuid, lms_user_id, content_key):
         """
         TODO: add docstring.
         """
-        raise NotImplementedError
+        query_params = {
+            'lms_user_id': lms_user_id,
+            'content_key': content_key,
+        }
+        response = self.client.get(
+            self.SUBSIDIES_ENDPOINT + f'{subsidy_uuid}/can_redeem/',
+            params=query_params,
+        )
+        response.raise_for_status()
+        return response.json()
