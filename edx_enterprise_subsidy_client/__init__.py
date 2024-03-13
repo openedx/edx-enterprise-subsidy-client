fc1ea00b839fe2105077ffa3922773702c3b8b7f
Muhammad Umar Khan <m.umarkhan999@gmail.com>
2024-03-13 15:37:54 +0500

    feat: add python312 support

--- edx_enterprise_subsidy_client/__init__.py
+++ edx_enterprise_subsidy_client/__init__.py
@@ -2,6 +2,6 @@
 Client for interacting with the enterprise-subsidy service..
 """
 
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 from .client import EnterpriseSubsidyAPIClient, EnterpriseSubsidyAPIClientV2, get_enterprise_subsidy_api_client
