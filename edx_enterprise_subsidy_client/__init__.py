d3a49ef5ed64203d78f2b1273a168123d2ddfcf4
Hamzah Ullah <hamzahullah@yahoo.com>
2025-04-14 15:12:07 -0400

    chore: Update version to 2.0.2

--- edx_enterprise_subsidy_client/__init__.py
+++ edx_enterprise_subsidy_client/__init__.py
@@ -2,6 +2,6 @@
 Client for interacting with the enterprise-subsidy service..
 """
 
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 
 from .client import EnterpriseSubsidyAPIClient, EnterpriseSubsidyAPIClientV2, get_enterprise_subsidy_api_client
