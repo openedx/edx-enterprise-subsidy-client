da3f3702bf7d7bf3ae03f1dcf7e9513dc4e8189c
Alexander Dusenbery <adusenbery@2u.com>
2025-05-06 11:24:48 -0400

    build: bump version

--- edx_enterprise_subsidy_client/__init__.py
+++ edx_enterprise_subsidy_client/__init__.py
@@ -2,6 +2,6 @@
 Client for interacting with the enterprise-subsidy service..
 """
 
-__version__ = '2.0.3'
+__version__ = '2.0.4'
 
 from .client import EnterpriseSubsidyAPIClient, EnterpriseSubsidyAPIClientV2, get_enterprise_subsidy_api_client
